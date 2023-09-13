bef4bf8142b054f23f175a9c373cc7ad8a197bf9
Gefanxe <actfire@gmail.com>
2023-09-13 09:30:54 +0000

    Deploying to gh-pages from @ Gefanxe/parcel-ts-web@1a010ffaee79a36bc833dbc9b686a99ea74d30e5 🚀

---
+++ index.1a4ac076.js
@@ -0,0 +1,3 @@
+console.log("Hello world! Parcel");//# sourceMappingURL=index.1a4ac076.js.map
+
+//# sourceMappingURL=index.1a4ac076.js.map
---
+++ index.c4405624.js
@@ -0,0 +1,3 @@
+console.log("Hello world! Parcel");//# sourceMappingURL=index.c4405624.js.map
+
+//# sourceMappingURL=index.c4405624.js.map
